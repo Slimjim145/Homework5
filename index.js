a7b35550400ca182f7e7fe51bca97d91023997ba
Slimjim <fang241999@gmail.com>
2020-07-24 16:48:22 -0400

    Changed JS files for added time slots, added bootstrap cdn link.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-let workDay = {
+let weekDay = {
+    "12 AM": "",
+    "1 AM": "",
+    "2 AM": "",
+    "3 AM": "",
+    "4 AM": "",
+    "5 AM": "",
+    "6 AM": "",
+    "7 AM": "",
     "8 AM": "",
     "9 AM": "",
     "10 AM": "",
@@ -9,13 +17,19 @@ let workDay = {
     "3 PM": "",
     "4 PM": "",
     "5 PM": "",
+    "6 PM": "",
+    "7 PM": "",
+    "8 PM": "",
+    "9 PM": "",
+    "10 PM": "",
+    "11PM": "",
   };
   
   $(document).ready(function(){
-    if(!localStorage.getItem('workDay')) {
+    if(!localStorage.getItem('weekDay')) {
       updateCalendarTasks(workDay);
     } else {
-      updateCalendarTasks(JSON.parse(localStorage.getItem('workDay')));
+      updateCalendarTasks(JSON.parse(localStorage.getItem('weekDay')));
     }
   })
   
@@ -48,6 +62,14 @@ let workDay = {
   
   function hourNumberFromHourString(hourString) {
     switch(hourString) {
+      case "12 AM": return 0;
+      case "1 AM": return 1;
+      case "2 AM": return 2;
+      case "3 AM": return 3;
+      case "4 AM": return 4;
+      case "5 AM": return 5;
+      case "6 AM": return 6;
+      case "7 AM": return 7;
       case "8 AM": return 8;
       case "9 AM": return 9;
       case "10 AM": return 10;
@@ -58,28 +80,34 @@ let workDay = {
       case "3 PM": return 15;
       case "4 PM": return 16;
       case "5 PM": return 17;
+      case "6 PM": return 18; 
+      case "7 PM": return 19;
+      case "8 PM": return 20;
+      case "9 PM": return 21;
+      case "10 PM": return 22;
+      case "11 PM": return 23;
     }
   }
   
   function loadCorrectDataset() {
-    result = localStorage.getItem('workDay')
+    result = localStorage.getItem('weekDay')
     return (result ? result : workDay);
   }
   
   function initializeLocalStorage() {
-    localStorage.setItem('workDay', JSON.stringify(workDay));
+    localStorage.setItem('weekDay', JSON.stringify(workDay));
   };
   
   function saveToLocalStorage(dayObj) {
-    localStorage.setItem('workDay', JSON.stringify(dayObj));
+    localStorage.setItem('weekDay', JSON.stringify(dayObj));
   }
   
   function saveSchedule(hourString, val) {
-    if(!localStorage.getItem('workDay')) {
+    if(!localStorage.getItem('weekDay')) {
       initializeLocalStorage();
     }
   
-    let workHours = JSON.parse(localStorage.getItem('workDay'));
+    let workHours = JSON.parse(localStorage.getItem('weekDay'));
     workHours[hourString] = val
   
     saveToLocalStorage(workHours);
